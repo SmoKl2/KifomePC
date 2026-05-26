15fa9d37616e3238c1ad723f15a90582af29671f
JulioCesar <oliveircesar459@gmail.com>
2026-05-26 09:48:58 -0300

    erro exe

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageTk
 import importlib.util
 import sys
 import os
-
+import sqlite3
 
 def resource_path(relative_path):
     try:
@@ -16,31 +16,49 @@ def montar_tela(frame, voltar, janela_principal):
     for widget in frame.winfo_children():
         widget.destroy()
 
+    cor_fundo = "#FCB57D"
+
     frame.config(bg="#FCB57D")
 
 #Guardar lista de imagens caso não exista
     if not hasattr(janela_principal, 'lista_imagens'):
         janela_principal.lista_imagens = []
 
-#Código nome kifome
-    imagem_original = Image.open(r"Imagens\nome kifome.png").convert("RGBA")
-    imagem_original = imagem_original.resize((600, 220))
-    imagem_menu = ImageTk.PhotoImage(imagem_original)
-    janela_principal.lista_imagens.append(imagem_menu)
+    conn = sqlite3.connect(resource_path("cardapio.db"))  # MUDOU AQUI
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS produtos (
+            id INTEGER PRIMARY KEY,
+            nome TEXT,
+            preco REAL,
+            descricao TEXT,
+            imagem TEXT
+        )
+    """)
+    conn.commit()
 
-    label_imagem = tk.Label(
-        frame,
-        image=imagem_menu,
-        bg="#FCB57D",
-    )
-    label_imagem.place(relx=0.5, rely=0.16, anchor="center")
+
+#Código nome kifome
+    caminho_imagem = resource_path(r"Imagens\nome kifome.png")  # MUDOU AQUI
+    if os.path.exists(caminho_imagem):
+        try:
+            img_original = Image.open(caminho_imagem).convert("RGBA")
+            img_original = img_original.resize((600, 220))
+            img_tk = ImageTk.PhotoImage(img_original)
+            janela_principal.lista_imagens.append(img_tk)
+            label_img = tk.Label(frame, image=img_tk, bg=cor_fundo)
+
+            label_img.pack(pady=10)
+
+        except:
+            pass
 
 #Código botão Menu
     def abrir_menu():
         for widget in frame.winfo_children():
             widget.destroy()
 
-        caminho = r"Menu.py"
+        caminho = resource_path("Menu.py")
         try:
             if "Menu" in sys.modules:
                 del sys.modules["Menu"]
@@ -48,16 +66,12 @@ def montar_tela(frame, voltar, janela_principal):
             spec = importlib.util.spec_from_file_location("Menu", caminho)
             modulo = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(modulo)
+            modulo.montar_tela(frame, voltar, janela_principal)
 
-            modulo.montar_tela(
-                frame=frame,
-                voltar=lambda: montar_tela(frame, voltar, janela_principal),
-                janela_principal=janela_principal
-            )
         except Exception as e:
             tk.Label(frame, text=f"Erro: {e}", fg="red", bg="#FCB57D").pack()
 
-    imagem_original = Image.open(r"Imagens\Menu.png").convert("RGBA")
+    imagem_original = Image.open(resource_path(r"Imagens\Menu.png"))
     imagem_original = imagem_original.resize((585, 500))
     imagem_botao = ImageTk.PhotoImage(imagem_original)
     janela_principal.lista_imagens.append(imagem_botao)
@@ -97,7 +111,7 @@ def montar_tela(frame, voltar, janela_principal):
         except Exception as e:
             tk.Label(frame, text=f"Erro: {e}", fg="red", bg="#FCB57D").pack()
 
-    imagem_original = Image.open(r"Imagens\Carrinho.png").convert("RGBA")
+    imagem_original = Image.open(resource_path(r"Imagens\Carrinho.png"))
     imagem_original = imagem_original.resize((585, 500))
     imagem_botao = ImageTk.PhotoImage(imagem_original)
     janela_principal.lista_imagens.append(imagem_botao)
@@ -137,7 +151,7 @@ def montar_tela(frame, voltar, janela_principal):
         except Exception as e:
             tk.Label(frame, text=f"Erro: {e}", fg="red", bg="#FCB57D").pack()
 
-    imagem_original = Image.open(r"Imagens\Configurações.png").convert("RGBA")
+    imagem_original = Image.open(resource_path(r"Imagens\Configurações.png"))
     imagem_original = imagem_original.resize((585, 500))
     imagem_botao = ImageTk.PhotoImage(imagem_original)
     janela_principal.lista_imagens.append(imagem_botao)
@@ -159,7 +173,11 @@ def montar_tela(frame, voltar, janela_principal):
     rodape = tk.Frame(frame, bg="white", height=55)
     rodape.place(relx=0, rely=1, anchor="sw", relwidth=1)
 
-#Texto ALPHA VERSION
+    def ao_sair():
+        conn.close()
+        voltar()
+
+    #Texto ALPHA VERSION
     texto = tk.Label(
         frame,
         text="ALPHA VERSION 1.0",
@@ -170,6 +188,7 @@ def montar_tela(frame, voltar, janela_principal):
         pady=0
     )
     texto.place(relx=0.07, rely=0.977, anchor="center")
+
 
 #Configurações janela
 
